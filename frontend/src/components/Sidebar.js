7e1781024059729111afd37e4aac59f1a6d46722
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2021-07-06 22:59:23 +0600

    bug fix

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,13 +1,17 @@
-import 'react-bootstrap-drawer/lib/style.css';
-import React, {useState} from 'react';
-import {Col, Collapse, Container, Row} from 'react-bootstrap';
-import {Drawer} from 'react-bootstrap-drawer';
+import "react-bootstrap-drawer/lib/style.css";
+import React, { useState } from "react";
+import { Col, Collapse, Container, Row } from "react-bootstrap";
+import { Drawer } from "react-bootstrap-drawer";
 
-const Sidebar = ({props,appliedbtn}) => {
+const Sidebar = ({ props, appliedbtn }) => {
   const [open, setOpen] = useState(false);
   const handleToggle = () => setOpen(!open);
+
+  const handleSelectedBtn = () => {
+    console.log("working");
+    appliedbtn("addType")};
   return (
-    <Drawer {...props} style={{backgroundColor: 'red'}}>
+    <Drawer {...props} style={{ backgroundColor: "red" }}>
       <Drawer.Toggle onClick={handleToggle} className="mr-auto" />
 
       <Collapse in={open}>
@@ -15,8 +19,8 @@ const Sidebar = ({props,appliedbtn}) => {
           {/* <Drawer.ToC> */}
           {/* <Drawer.Header href="/">Application</Drawer.Header> */}
 
-          <Drawer.Nav >
-            <Drawer.Item onClick={()=>appliedbtn('addType')}>Add Type</Drawer.Item>
+          <Drawer.Nav>
+            <Drawer.Item ><button onClick={handleSelectedBtn}>Add Type</button></Drawer.Item>
             <Drawer.Item href="/settings">Add Category</Drawer.Item>
             <Drawer.Item href="/settings">Add Food</Drawer.Item>
             <Drawer.Item href="/settings">All Food</Drawer.Item>
