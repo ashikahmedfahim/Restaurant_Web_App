ce79500f0f19242a752f11dd045f18dda7954427
Nayeem Ahmed <45434391+Nayeemr45@users.noreply.github.com>
2021-07-25 04:25:51 +0600

    update

--- frontend/src/components/Admin/AddFood.js
+++ frontend/src/components/Admin/AddFood.js
@@ -21,23 +21,19 @@ const AddFood = ({ history }) => {
 
   const foodAdd = useSelector((state) => state.foodAdd);
   const { loading, success, error, FOOD } = foodAdd;
-  
+
   const allCategory = useSelector((state) => state.allCategory);
   const { categoryloading, categoryerror, AllCategoryInfo } = allCategory;
-const Category = AllCategoryInfo;
+  const Category = AllCategoryInfo;
 
-
-console.log(AllCategoryInfo)
   const handleChange = (e) =>
     setFood({ ...food, [e.target.name]: e.target.value });
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("food---------", food);
     dispatch(addFood(food, history));
 
     setFood(initialState);
   };
-
 
   useEffect(() => {
     dispatch(getCategory());
@@ -45,66 +41,66 @@ console.log(AllCategoryInfo)
 
   return (
     <>
-    {
-      categoryloading?
-      <>
-      <Loading/>
-      </>:
-    <>
-      {loading ? (
+      {categoryloading ? (
         <>
           <Loading />
         </>
       ) : (
         <>
-          <Row className="px-3">
-            {success ? (
-              <>
-                <ErrorMessage
-                  variant="success"
-                  message={`Successfully Added.`}
-                />
-              </>
-            ) : (
-              <></>
-            )}
-            {error ? (
-              <>
-                <ErrorMessage variant="danger" message={error} />
-              </>
-            ) : (
-              <></>
-            )}
-            <h2 className="py-3">Add Food</h2>
-            <Form onSubmit={handleSubmit}>
-              <Row>
-                <Col sm={12} md={6} lg={6} xl={6}>
-                  <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlInput1"
-                  >
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                      type="text"
-                      name="name"
-                      onChange={handleChange}
-                      placeholder="Enter food name"
+          {loading ? (
+            <>
+              <Loading />
+            </>
+          ) : (
+            <>
+              <Row className="px-3">
+                {success ? (
+                  <>
+                    <ErrorMessage
+                      variant="success"
+                      message={`Successfully Added.`}
                     />
-                  </Form.Group>
+                  </>
+                ) : (
+                  <></>
+                )}
+                {error ? (
+                  <>
+                    <ErrorMessage variant="danger" message={error} />
+                  </>
+                ) : (
+                  <></>
+                )}
+                <h2 className="py-3">Add Food</h2>
+                <Form onSubmit={handleSubmit}>
+                  <Row>
+                    <Col sm={12} md={6} lg={6} xl={6}>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlInput1"
+                      >
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                          type="text"
+                          name="name"
+                          onChange={handleChange}
+                          placeholder="Enter food name"
+                        />
+                      </Form.Group>
 
-                  <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlInput1"
-                  >
-                    <Form.Label>Price</Form.Label>
-                    <Form.Control
-                      type="number"
-                      name="price"
-                      onChange={handleChange}
-                      placeholder="Enter food price"
-                    />
-                  </Form.Group>
-                  {/*   <Form.Group className="mb-3" controlId="exampleForm.ControlSelect1">
+                      <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlInput1"
+                      >
+                        <Form.Label>Price</Form.Label>
+                        <Form.Control
+                          type="number"
+                          name="price"
+                          onChange={handleChange}
+                          placeholder="Enter food price"
+                        />
+                      </Form.Group>
+                      {/*   <Form.Group className="mb-3" controlId="exampleForm.ControlSelect1">
               <Form.Label>Select Type</Form.Label>
               <Form.Control as="select" name="type" onChange={handleChange}>
                 <option value="">Choose a type</option>
@@ -114,98 +110,96 @@ console.log(AllCategoryInfo)
                 ))}
               </Form.Control> 
             </Form.Group>*/}
-                  <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlSelect1"
-                  >
-                    <Form.Label>Select Category</Form.Label>
-                    <Form.Control
-                      as="select"
-                      name="category"
-                      onChange={handleChange}
-                    >
-                      <option value="">Choose a category</option>
-
-                      {Object.keys(Category).map((item) => (
-                        <option
-                          key={Category[item]._id}
-                          value={Category[item]._id}
+                      <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlSelect1"
+                      >
+                        <Form.Label>Select Category</Form.Label>
+                        <Form.Control
+                          as="select"
+                          name="category"
+                          onChange={handleChange}
                         >
-                          {Category[item].name}
-                        </option>
-                      ))}
-                    </Form.Control>
-                  </Form.Group>
-                  <Form.Group className="mb-3">
-                    <Form.Label className="mr-2">InStock :</Form.Label>
+                          <option value="">Choose a category</option>
 
-                    <Form.Check
-                      inline
-                      label="Yes"
-                      value={true}
-                      name="inStock"
-                      type="radio"
-                      onChange={handleChange}
-                    />
-                    <Form.Check
-                      inline
-                      label="No"
-                      value={false}
-                      name="inStock"
-                      type="radio"
-                      onChange={handleChange}
-                    />
-                  </Form.Group>
-                </Col>
-                <Col sm={12} md={6} lg={6} xl={6}>
-                  
-                  <Form.Group controlId="formFileSm" className="mb-3">
-                    <Form.Label>Add Image :</Form.Label>
-                    <Form.Control
-                      type="file"
-                      name="image"
-                      size="sm"
-                      onChange={handleChange}
-                    />
-                  </Form.Group>
+                          {Object.keys(Category).map((item) => (
+                            <option
+                              key={Category[item]._id}
+                              value={Category[item]._id}
+                            >
+                              {Category[item].name}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Label className="mr-2">InStock :</Form.Label>
 
-                  <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlTextarea1"
-                  >
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      name="description"
-                      rows={3}
-                      onChange={handleChange}
-                    />
-                  </Form.Group>
-                  <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlInput1"
-                  >
-                    <Form.Label>Discount</Form.Label>
-                    <Form.Control
-                      type="number"
-                      name="discount"
-                      placeholder="Add Discount"
-                      onChange={handleChange}
-                    />
-                  </Form.Group>
-                  <Button variant="dark" type="submit">
-                    Submit
-                  </Button>
-                </Col>
+                        <Form.Check
+                          inline
+                          label="Yes"
+                          value={true}
+                          name="inStock"
+                          type="radio"
+                          onChange={handleChange}
+                        />
+                        <Form.Check
+                          inline
+                          label="No"
+                          value={false}
+                          name="inStock"
+                          type="radio"
+                          onChange={handleChange}
+                        />
+                      </Form.Group>
+                    </Col>
+                    <Col sm={12} md={6} lg={6} xl={6}>
+                      <Form.Group controlId="formFileSm" className="mb-3">
+                        <Form.Label>Add Image :</Form.Label>
+                        <Form.Control
+                          type="file"
+                          name="image"
+                          size="sm"
+                          onChange={handleChange}
+                        />
+                      </Form.Group>
+
+                      <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlTextarea1"
+                      >
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                          as="textarea"
+                          name="description"
+                          rows={3}
+                          onChange={handleChange}
+                        />
+                      </Form.Group>
+                      <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlInput1"
+                      >
+                        <Form.Label>Discount</Form.Label>
+                        <Form.Control
+                          type="number"
+                          name="discount"
+                          placeholder="Add Discount"
+                          onChange={handleChange}
+                        />
+                      </Form.Group>
+                      <Button variant="dark" type="submit">
+                        Submit
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form>
               </Row>
-            </Form>
-          </Row>
+            </>
+          )}
         </>
       )}
-      </>
-    }
     </>
-    
   );
 };
 
