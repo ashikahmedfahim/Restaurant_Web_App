5821daec5b1d2b64ddb1fad0771f95446d0f44dc
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2021-08-04 12:01:18 +0600

    order route updated and controller added

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const order = require("../controllers/orders")
+const catchAsync = require("../utilities/catchAsync");
 const { isAuthenticated, isAdmin } = require("../middlewares/index");
 
-router.get("/:id/orders", (req, res, next) => {});
-router.post(":id/orders", (req, res, next) => {});
-router.get("/:id/orders/:id", (req, res, next) => {});
-router.delete("/:id/orders/:id", (req, res, next) => {});
+router.get("/:id/orders", catchAsync(order.getAll));
+router.post(":id/orders", catchAsync(order.create));
+router.get("/:id/orders/:id", catchAsync(order.getOne));
 
 module.exports = router;
