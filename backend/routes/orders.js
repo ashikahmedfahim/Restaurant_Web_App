4fbcf0442945d5a2a76e0dbd67640e08d4849dfe
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2021-08-08 21:51:15 +0600

    order authentication added

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const order = require("../controllers/orders")
+const order = require("../controllers/orders");
 const catchAsync = require("../utilities/catchAsync");
 const { isAuthenticated, isAdmin } = require("../middlewares/index");
 
-router.get("/:id/orders", catchAsync(order.getAll));
-router.post(":id/orders", catchAsync(order.create));
-router.get("/:id/orders/:id", catchAsync(order.getOne));
+
+//need aithorization here
+router.get("/:id/orders", isAuthenticated, catchAsync(order.getAll));
+router.post(":id/orders", isAuthenticated, catchAsync(order.create));
+router.get("/:id/orders/:id", isAuthenticated, catchAsync(order.getOne));
 
 module.exports = router;
