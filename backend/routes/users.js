b27ab1c12770fe761e9a36e0f800c4e48576883c
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2021-08-15 23:44:36 +0600

    authorization added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,12 +2,21 @@ const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utilities/catchAsync");
 const user = require("../controllers/users");
-const { isAuthenticated, isAdmin } = require("../middlewares/index");
+const {
+  isAuthenticated,
+  isAdmin,
+  isAuthorized,
+} = require("../middlewares/index");
 
 router.get("/", isAuthenticated, isAdmin, catchAsync(user.getAll));
 router.post("/", catchAsync(user.createOne));
-router.get("/:id", isAuthenticated, catchAsync(user.getOne));
-router.put("/:id", isAuthenticated, catchAsync(user.updateOne));
-router.delete("/:id", isAuthenticated, catchAsync(user.deleteOne));
+router.get("/:id", isAuthenticated, isAuthorized, catchAsync(user.getOne));
+router.put("/:id", isAuthenticated, isAuthorized, catchAsync(user.updateOne));
+router.delete(
+  "/:id",
+  isAuthenticated,
+  isAuthorized,
+  catchAsync(user.deleteOne)
+);
 
 module.exports = router;
